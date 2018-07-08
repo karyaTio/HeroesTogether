8e1a7e5f83b730d30817e2e4777cbf03c2c81203
unknown <ragilsatrio20@gmail.com>
2018-07-08 11:19:04 +0700

    sharing folder

---
+++ application/views/layout/Header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Heroes</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php <?php echo base_url('assets/css/bootstrap-theme.min.css') ?>">
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+	
+<div class="navbar navbar-default">
+	<div class="container">
+		<h2><span class="glyphicon glyphicon-home"></span>&nbsp;Welcome to MarvelsWeb</h2>
+	</div>
+</div>
+<div class="container">
